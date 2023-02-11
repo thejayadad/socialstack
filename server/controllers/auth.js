1e863b5387c2085139f2dbc46782dfbf70b28f43
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-11 17:00:28 -0500

    update

---
+++ server/controllers/auth.js
@@ -0,0 +1,36 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+
+import User from "../models/User";
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            favoritelocation,
+            hobby
+        } = req.body;
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            favoritelocation,
+            hobby,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        })
+    } catch (error) {
+
+    }
+}
