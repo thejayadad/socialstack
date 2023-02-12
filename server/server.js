d91f4ebf2b3efe2d4067c532ce8e3e962e444976
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-11 22:49:16 -0500

    update

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,7 +24,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-import { register } from "./controllers/auth.js";
 
 
 const storage = multer.diskStorage({
@@ -38,7 +38,7 @@ const storage = multer.diskStorage({
 
   //ROUTES WITH AUTH FILES
   app.post("/auth/register", upload.single("picture"), register);
-
+  app.use("/auth", authRoutes);
 
 
   //MONGODB CONNECTION
